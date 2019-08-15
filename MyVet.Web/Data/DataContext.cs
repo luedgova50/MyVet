ab9935183082f213ebcc00e7f967ad2fc1328bcd
Luis Eduardo Gonzàelez  Vargas <luedgova1967@gmail.com>
2019-08-14 22:01:55 -0500

    Add Tables: Pets, Owners, Histories, PetTypes, Agendas and ServiceTypes; Create SeddDb and Modify Program Class

---
+++ MyVet.Web/Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿namespace MyVet.Web.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using MyVet.Web.Data.Entities;
+
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Owner> Owners { get; set; }
+
+        public DbSet<PetType> PetTypes { get; set; }
+
+        public DbSet<Pet> Pets { get; set; }
+
+        public DbSet<ServiceType> ServiceTypes { get; set; }
+
+        public DbSet<History> Histories { get; set; }
+
+        public DbSet<Agenda> Agendas { get; set; }
+
+    }
+}
--- MyVet.Web/Startup.cs
+++ MyVet.Web/Startup.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace MyVet.Web
+﻿namespace MyVet.Web
 {
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using MyVet.Web.Data;
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -28,9 +25,19 @@ namespace MyVet.Web
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
+
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddDbContext<DataContext>(cfg =>
+            {
+                cfg.UseSqlServer(
+                    Configuration.
+                    GetConnectionString(
+                        "DefaultConnection"));
+            });
+
+            services.AddTransient<SeedDb>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
@@ -45,11 +52,14 @@ namespace MyVet.Web
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
+
             app.UseStaticFiles();
+
             app.UseCookiePolicy();
 
             app.UseMvc(routes =>
